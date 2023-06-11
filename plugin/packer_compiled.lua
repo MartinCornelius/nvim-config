3174c02440f8d27eb939ef622a372684c7856d30
Martin Cornelius <62438208+MartinCornelius@users.noreply.github.com>
2023-06-11 12:24:45 +0200

    packer

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -184,6 +184,11 @@ _G.packer_plugins = {
     path = "C:\\Users\\Martin Cornelius\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
+  ["toggleterm.nvim"] = {
+    loaded = true,
+    path = "C:\\Users\\Martin Cornelius\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\toggleterm.nvim",
+    url = "https://github.com/akinsho/toggleterm.nvim"
+  },
   ["tokyonight.nvim"] = {
     loaded = true,
     path = "C:\\Users\\Martin Cornelius\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\tokyonight.nvim",
